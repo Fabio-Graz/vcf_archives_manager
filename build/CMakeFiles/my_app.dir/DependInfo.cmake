fef41edb6d54d22a2ed487355c9a3692ba64a447
Fabio Grazioso <fabio.forall@gmail.com>
2025-03-07 12:11:47 +0500

    first commit

---
+++ build/CMakeFiles/my_app.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/fabio/Documents/programming/C++/Esperimenti_GTK/working_projects/merge_vcf_archives/main.cpp" "CMakeFiles/my_app.dir/main.cpp.o" "gcc" "CMakeFiles/my_app.dir/main.cpp.o.d"
+  "/home/fabio/Documents/programming/C++/Esperimenti_GTK/working_projects/merge_vcf_archives/myWindow.cpp" "CMakeFiles/my_app.dir/myWindow.cpp.o" "gcc" "CMakeFiles/my_app.dir/myWindow.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
